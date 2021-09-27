b76b0433c65eb467f8acd6c2f706565390e28d07
moath515 <91217801+moath515@users.noreply.github.com>
2021-09-27 22:00:26 +0300

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,1 +1,23 @@
-
+import java.util.Scanner;
+class Main {
+ public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    float num1;
+    float num2;
+    System.out.println("Enter a num");
+    num1 = myObj.nextfloat();
+    System.out.println("Enter a num");
+    num2 = myObj.nextfloat();
+    System.out.println(num1+num2);
+    System.out.println(num1-num2);
+    System.out.println(num1*num2);
+    System.out.println(num1/num2);
+    System.out.println(num1%num2);
+    System.out.println(num1>num2);
+    System.out.println(num1>=num2);
+    System.out.println(num1<num2);
+    System.out.println(num1<=num2);
+    System.out.println(num1!=num2);
+    System.out.println(num1==num2);
+   }
+ }
