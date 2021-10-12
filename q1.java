611f438f2169cb0f78f7fce63a207cbc7d294dee
442013011 <91217801+442013011@users.noreply.github.com>
2021-10-12 09:16:43 +0300

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q1{
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.
+ 
+ int = num1
+
+ System.Scanner.out.println();
+    
+    System.out.println(odd);
+    
+    
+    System.out.println(Even);
+    }}}
