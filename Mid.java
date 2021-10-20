61e32dbd6a0dd8687bf0c2201ba2756ad4957b38
442013011 <91217801+442013011@users.noreply.github.com>
2021-10-20 21:01:19 +0300

    Create Mid.java

---
+++ Mid.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Mid 
+{
+public static void main (String[] args){
+    Scanner scan = new Scanner(System.in);
+    int num;
+    
+    System.out.println("Enter 0 or 10");
+    
+    
+    
+float num1;
+
+num1 = scan.nextFloat();
+    
+   
+if(num1 < 0 && 100>num1);{
+    System.out.println("Error");
+ } else { (50>num1);
+System.out.println("");
+     
+ }else{(num1>50);
+ System.out.println("")
+
+
+    
+    
+}
+}
