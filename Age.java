2753c24f16ca5579e79884a686b4c4844a244a9d
442013011 <91217801+442013011@users.noreply.github.com>
2021-12-14 21:58:14 +0300

    Update Age.java

--- Age.java
+++ Age.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 class Main {
   public static void main(String [] args){
     Scanner scan = new Scanner(System.in);
-    System.out.println("Enter  your brith year ");
-    int brith; 
-    brith=scan.nextInt();
+    System.out.println("Enter  your ss year ");
+    int ss; 
+    ss=scan.nextInt();
      int age;
-     age = 2021- brith;
+     age = 2021- ss;
      System.out.println( age);}}
