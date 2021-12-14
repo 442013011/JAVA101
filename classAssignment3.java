81e6288b58b2a6e5ded034e46e75571b5b54abca
442013011 <91217801+442013011@users.noreply.github.com>
2021-12-14 22:00:14 +0300

    Update classAssignment3.java

--- classAssignment3.java
+++ classAssignment3.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  [4]:Faith has the highest 70 grade in math
  [2]:Jodi has the lowest 5 grade in math
  */
-public classAssignment3{
+public class Assignment3{
         /**
          * This function will help you to cast string into int
          */
