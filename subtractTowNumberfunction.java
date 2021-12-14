caac1e498db1906c4a91c75065fa06edf594c440
442013011 <91217801+442013011@users.noreply.github.com>
2021-12-14 20:16:41 +0300

    Create subtractTowNumberfunction.java

---
+++ subtractTowNumberfunction.java
@@ -0,0 +1,14 @@
+public class subtractTowNumberfunction {
+  static int subtract(int k, int j ) {
+    return k - j;
+  }
+  
+ 
+  
+  public static void main(String[] args) {
+    int M10 = subtract(76, 85);
+ 
+    System.out.println( M10);
+    
+  }
+}
