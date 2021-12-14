cf38cc8a6c781092e5d99367c62a60e8a21cb571
442013011 <91217801+442013011@users.noreply.github.com>
2021-12-14 20:08:59 +0300

    Create stars.java

---
+++ stars.java
@@ -0,0 +1,11 @@
+class stars{
+  public static void main(String [] args){
+     
+       for (int   i = 0 ;   i< 5 ; i ++){
+       System.out.println("");
+       for (int stars=0 ; stars < 5-i; stars ++ ){
+       System.out.print("*");}}
+      
+  }
+    
+}
