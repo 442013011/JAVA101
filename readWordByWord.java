dce0b313f265706581b571df863a005d3c715d41
442013011 <91217801+442013011@users.noreply.github.com>
2021-12-15 04:24:49 +0300

    Create readWordByWord.java

---
+++ readWordByWord.java
@@ -0,0 +1,10 @@
+public class readWordByWord {
+        public static void main(String[] args) {
+                String sxb = "waleed\nalshrhri";
+
+                System.out.println(sxb);
+
+
+
+        }
+}
