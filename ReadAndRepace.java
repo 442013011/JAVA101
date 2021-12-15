29f5cd0c787477a5f75d81d0b4fe8aa663ed0c50
442013011 <91217801+442013011@users.noreply.github.com>
2021-12-15 14:13:22 +0300

    Create ReadAndRepace.java

---
+++ ReadAndRepace.java
@@ -0,0 +1,11 @@
+public class ReadAndRepace{
+public static void main(String[] args) {
+     String name = "moathalayafi";
+     for (int x =0;  x<=8 ; ++x){
+         if(name.charAt(x) == 'a'){
+        System.out.println('x');}
+        else {
+            System.out.println(name.charAt(x));
+        }
+        
+     }}}
